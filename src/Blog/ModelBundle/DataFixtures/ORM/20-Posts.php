d1a8f03da7cc8b08d967700ce2194cffe2308d61
Marko Jankovic <markoj@vast.com>
2014-07-10 20:31:54 +0200

    fixed Posts fixture, added new type of post and get User model by email

--- src/Blog/ModelBundle/DataFixtures/ORM/20-Posts.php
+++ src/Blog/ModelBundle/DataFixtures/ORM/20-Posts.php
@@ -44,8 +44,14 @@ class Posts extends AbstractFixture implements OrderedFixtureInterface{
         $p2->setBody("went to either the first or second Carsonified Future of Web Apps/Design conferences -- I think it was back in 2005, so over seven years ago now. I've been thinking a lot about that time and the things that used to preoccupy me:");
         $p2->setUser($this->getUser($manager, 'admin@example.com'));
 
+        $p3 = new Post();
+        $p3->setTitle('User post');
+        $p3->setBody("went to either the first or second Carsonified Future of Web Apps/Design conferences -- I think it was back in 2005, so over seven years ago now. I've been thinking a lot about that time and the things that used to preoccupy me:");
+        $p3->setUser($this->getUser($manager, 'user@example.com'));
+
         $manager->persist($p1);
         $manager->persist($p2);
+        $manager->persist($p3);
         $manager->flush();
     }
 
@@ -57,10 +63,12 @@ class Posts extends AbstractFixture implements OrderedFixtureInterface{
      *
      * @return User
      */
-    private function getUser(ObjectManager $manager, $username)
+    private function getUser(ObjectManager $manager, $email)
     {
-        return $manager
-            ->getRepository("ModelBundle:User")
-            ->loadUserByUsername( 'admin@example.com' );
+        return $manager->getRepository("ModelBundle:User")->findOneBy(
+            array(
+               'email' => $email
+            )
+        );
     }
 }
